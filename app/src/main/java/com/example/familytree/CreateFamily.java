9bfda6fd8c9f8e8d355e4d7cd7a706b715c1a645
Janani <jananiprakash@gmail.com>
2020-06-19 14:12:34 -0500

    Did the xml for create

--- app/src/main/java/com/example/familytree/CreateFamily.java
+++ app/src/main/java/com/example/familytree/CreateFamily.java
@@ -4,14 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class CreateFamily extends AppCompatActivity {
+
+    EditText familyName;
+    Spinner cloudStorage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_screen);
+        familyName = (EditText) findViewById(R.id.familyName);
+        cloudStorage = (Spinner) findViewById(R.id.cloudStorage);
+
+        Intent intent = new Intent(getIntent());
     }
 }
