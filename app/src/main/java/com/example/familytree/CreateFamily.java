5185ea0f17d92cd5ae82e229fa38d43c9917ef5d
Harini <hariniprakash@gmail.com>
2020-06-18 20:33:42 -0500

    Initial commit

---
+++ app/src/main/java/com/example/familytree/CreateFamily.java
@@ -0,0 +1,18 @@
+package com.example.familytree;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class CreateFamily extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_screen);
+    }
+}
+ 
--- app/src/main/java/com/example/familytree/MainActivity.java
+++ app/src/main/java/com/example/familytree/MainActivity.java
@@ -2,9 +2,29 @@ package com.example.familytree;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton create_button = (ImageButton) findViewById(R.id.create_button);
+    ImageButton update_button = (ImageButton) findViewById(R.id.update_button);
+    ImageButton load_button = (ImageButton) findViewById(R.id.load_button);
+
+    public void createButtonClicked(View v) {
+        Intent createClicked = new Intent(MainActivity.this, CreateFamily.class);
+        startActivity(createClicked);
+    }
+
+    public void updateButtonClicked(View v) {
+
+    }
+
+    public void loadButtonClicked(View v) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
