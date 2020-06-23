7ed202b65d1cf0fe4cc36e8e99f263467741dbf7
Janani <jananiprakash@gmail.com>
2020-06-23 16:06:53 -0500

    Invite code load screen

--- app/src/main/java/com/example/familytree/MainActivity.java
+++ app/src/main/java/com/example/familytree/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     ImageButton load_button;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
