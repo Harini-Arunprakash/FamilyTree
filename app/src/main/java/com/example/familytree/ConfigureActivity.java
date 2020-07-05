7dd902b1cf6fd3b8b4e06a9d6177c1cb56d6610e
Janani <jananiprakash@gmail.com>
2020-07-04 19:09:39 -0500

    Trial camera

--- app/src/main/java/com/example/familytree/ConfigureActivity.java
+++ app/src/main/java/com/example/familytree/ConfigureActivity.java
@@ -3,12 +3,51 @@ package com.example.familytree;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CheckBox;
 
 public class ConfigureActivity extends AppCompatActivity {
+
+    CheckBox nameCheckBox;
+    CheckBox bioCheckBox;
+    CheckBox picCheckBox;
+    CheckBox contactCheckBox;
+    CheckBox birthdayCheckBox;
+    boolean name = false;
+    boolean bio = false;
+    boolean pic = false;
+    boolean contact = false;
+    boolean birthday = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configure);
+
+        nameCheckBox = findViewById(R.id.nameCheckBox);
+        bioCheckBox = findViewById(R.id.bioCheckBox);
+        picCheckBox = findViewById(R.id.picCheckBox);
+        contactCheckBox = findViewById(R.id.contactCheckBox);
+        birthdayCheckBox = findViewById(R.id.birthdayCheckBox);
+
+        if(nameCheckBox.isChecked()){
+            name = true;
+        }
+
+        if(bioCheckBox.isChecked()){
+            bio = true;
+        }
+
+        if(picCheckBox.isChecked()){
+            pic = true;
+        }
+
+        if(contactCheckBox.isChecked()){
+            contact = true;
+        }
+
+        if(birthdayCheckBox.isChecked()){
+            birthday = true;
+        }
+
     }
 }
